90ae9b3ed1a9b639e55dae5e2eac8b2d743cfffc
sumiravi <ravikannagi9@gmail.com>
2025-07-03 17:46:46 -1200

    Add files via upload

---
+++ contiunestatement.java
@@ -0,0 +1,9 @@
+public class contiunestatement {
+    public static void main(String[] args){
+        int age=12;
+        System.out.println("using return");
+        if(age<18)
+            return;
+        System.out.println("will not get executed");
+    }
+}
