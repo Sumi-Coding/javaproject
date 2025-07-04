6ff411c8a04e242efdbeb0f5396e6963f53d35eb
sumiravi <ravikannagi9@gmail.com>
2025-07-03 17:32:18 -1200

    Add files via upload

---
+++ multidimensional.java
@@ -0,0 +1,10 @@
+public class multidimensional {
+    public static void main(String[] args){
+        int arr[][]={{2,7,9},{3,6,1},{7,5,9}};
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++) {
+                System.out.println(arr[i][j] + "");
+            }
+        }
+    }
+}
