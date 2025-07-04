f831e90a122ecce289e5f3a94b09200a2b671569
sumiravi <ravikannagi9@gmail.com>
2025-07-03 17:35:03 -1200

    Add files via upload

---
+++ twodarraydeclae.java
@@ -0,0 +1,11 @@
+public class twodarraydeclae {
+    public static void main(String[] args){
+        String name[][]={{"ravi,kannagi"},{"pavi,abi"}};
+        System.out.println("enter the name");
+        for(int i=0;i<name.length;i++){
+            for(int j=0;i< name.length;i++){
+                System.out.println(name[i][j]+"\t");
+            }
+        }
+    }
+}
