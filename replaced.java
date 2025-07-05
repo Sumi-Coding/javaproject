457e150af013dbe4cfc7000885ed579a264cd2ea
sumiravi <ravikannagi9@gmail.com>
2025-07-05 02:58:15 -1200

    Add files via upload

---
+++ replaced.java
@@ -0,0 +1,8 @@
+public class replaced {
+    public static void main(String[] args){
+        String original="sumwthra Ravwchandran";
+        String replaced=original.replace("w","i");
+        System.out.println("original:"+original);
+        System.out.println("replaced:"+replaced);
+    }
+}
