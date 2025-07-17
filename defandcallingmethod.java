404790fdef2a48e965c7207d221e4daa1d5ce79b
sumiravi <ravikannagi9@gmail.com>
2025-07-17 12:20:36 +0530

    Add files via upload

---
+++ defandcallingmethod.java
@@ -0,0 +1,9 @@
+public class defandcallingmethod {
+    int add(int a,int b){
+        return a+b;
+    }
+   public static void main(String[] args){
+        defandcallingmethod c =new defandcallingmethod();
+        System.out.println("sum:"+c.add(4,7));
+   }
+}
