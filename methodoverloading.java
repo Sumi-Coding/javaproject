c22c10de75eb2f5042cd16efe313d1b44a6054a8
sumiravi <ravikannagi9@gmail.com>
2025-07-17 12:18:30 +0530

    Add files via upload

---
+++ methodoverloading.java
@@ -0,0 +1,8 @@
+public class methodoverloading {
+    void show(int a){
+        System.out.println("integer"+a);
+    }
+    void show(String s){
+        System.out.println("String:"+s);
+    }
+}
