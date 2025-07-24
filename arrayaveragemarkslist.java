05a367d149e6e306555227eaef67f09d2ba7989e
sumiravi <ravikannagi9@gmail.com>
2025-07-24 20:22:54 +0530

    Add files via upload

---
+++ arrayaveragemarkslist.java
@@ -0,0 +1,25 @@
+
+
+public class arrayaveragemarkslist {
+    public static void main(String[] args) {
+        int tamil=98;
+        int eng=98;
+        int maths=76;
+        int science=54;
+        int social=45;
+        int totalmarks=371;
+        int averagemarks=45;
+
+        System.out.println("tamil:"+tamil);
+        System.out.println("eng:"+eng);
+        System.out.println("maths:"+maths);
+        System.out.println("science:"+science);
+        System.out.println("social:"+social);
+        System.out.println("totalmark:"+totalmarks);
+        System.out.println("averagemarks:"+averagemarks);
+    }
+
+}
+
+
+
