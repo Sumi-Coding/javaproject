2c0c3d00f3dc43c58a095e616357ec7d6b775903
sumiravi <ravikannagi9@gmail.com>
2025-07-24 20:23:55 +0530

    Add files via upload

---
+++ arraycalculates.java
@@ -0,0 +1,18 @@
+public class arraycalculates {
+    public static void main(String[] args) {
+        int[] age = new int[5];
+        int average = age[0];
+        age = new int[5];
+        age[0] = 20;
+        age[1] = 30;
+        age[2] = 12;
+        age[3] = 45;
+        age[4] = 34;
+        for (int i = 0; i < average; i++) {
+            if (i < average) {
+                i = average;
+            }
+        }
+        System.out.println("the average is:" + average);
+    }
+}
