7472bc3d733e4e42fb22385801182e7659a42851
sumiravi <ravikannagi9@gmail.com>
2025-07-27 20:32:25 +0530

    Add files via upload

---
+++ framemaphashmap.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class framemaphashmap {
+    public static void main(String[] args){
+        Map<Integer,String>map=new HashMap<>();
+            map.put(1,"apple");
+            map.put(2,"banana");
+            map.put(3,"grapes");
+            System.out.println("Map:"+map);
+        }
+    }
+
