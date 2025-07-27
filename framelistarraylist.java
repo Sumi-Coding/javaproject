83afd287227b04da15e5bbb25a1e29070e6e177d
sumiravi <ravikannagi9@gmail.com>
2025-07-27 20:28:08 +0530

    Add files via upload

---
+++ framelistarraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class framelistarraylist {
+    public static void main(String[]args){
+        List<String>fruits=new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("apple");
+        System.out.println("fruits:"+fruits);
+    }
+
+
+}
