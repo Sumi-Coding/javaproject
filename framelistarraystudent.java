e6e2a127c9d8d227e875e6e855a9eec1f5470962
sumiravi <ravikannagi9@gmail.com>
2025-07-27 20:30:43 +0530

    Add files via upload

---
+++ framelistarraystudent.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class framelistarraystudent {
+    public static void main(String[] args){
+        List<String>student=new ArrayList<>();
+        student.add("sumithra");
+        student.add("sathya");
+        student.add("pavithra");
+        student.add("sumithra");
+        student.add("sathya");
+        System.out.println("student:"+student);
+    }
+}
