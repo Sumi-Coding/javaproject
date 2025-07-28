60ec00be3277a7096950942eb0799b73ff07bc09
sumiravi <ravikannagi9@gmail.com>
2025-07-28 12:47:44 +0530

    Add files via upload

---
+++ framecollectionshrtingarrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class framecollectionshrtingarrays {
+    public static void main(String[] args){
+      int[] num={1,2,3,4,5};
+        Arrays.sort(num);
+        System.out.println(Arrays.toString(num));
+    }
+}
