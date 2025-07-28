5721cf41fb1f2fb6106a4a93c4dbc8c5e03f8aca
sumiravi <ravikannagi9@gmail.com>
2025-07-28 12:50:39 +0530

    Add files via upload

---
+++ framelistmarkaverage.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class framelistmarkaverage {
+    public static void main(String[]args){
+        List<Integer>marks=new ArrayList<>()
+    }
+}
