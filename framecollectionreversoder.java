af0012bca31053ddd4943ef025b3318a7a39a7ff
sumiravi <ravikannagi9@gmail.com>
2025-07-28 12:45:29 +0530

    Add files via upload

---
+++ framecollectionreversoder.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class framecollectionreversoder {
+    public static void main(String[] args){
+        Integer[]num={1,2,3,4,5};
+        Arrays.sort(nums,collection);
+
+    }
+}
