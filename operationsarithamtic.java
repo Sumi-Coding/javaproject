d0334bd8dae68573d7bf258f0a72299d20ccba14
Sumithra.R <ravikannagi9@gmail.com>
2025-09-22 22:38:41 +0530

    Add files via upload

---
+++ operationsarithamtic.java
@@ -0,0 +1,16 @@
+public class operationsarithamtic {
+    public static void main(String[] args) {
+        int num = 25;
+        int num1 = 10;
+        System.out.println("===Arithametic operation===");
+        System.out.println("Frist Number:" + num);
+        System.out.println("Second Number:" + num1);
+        System.out.println("addition:" + (num +num1));
+        System.out.println("subtraction:" + (num -num1));
+        System.out.println("multiplication:" + (num * num1));
+        System.out.println("Division:" + (num / num1));
+        System.out.println("modulus:" + (num % num1));
+    }
+
+}
+
