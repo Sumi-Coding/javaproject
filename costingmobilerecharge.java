74eb76c6ab474daca17c2e5bbdd0bbebd6fd553b
Sumithra.R <ravikannagi9@gmail.com>
2025-09-22 22:34:06 +0530

    Add files via upload

---
+++ costingmobilerecharge.java
@@ -0,0 +1,12 @@
+public class costingmobilerecharge {
+    public static void main(String[] args){
+        long mobilenum=9876543210L;
+        double planamount=199.99;
+        int payableamount=(int) planamount;
+        System.out.println("===mobile recharge ===");
+        System.out.println("mobile number :"+mobilenum);
+        System.out.println("plan Amount:"+planamount);
+        System.out.println("payable Amount:"+payableamount);
+
+    }
+}
