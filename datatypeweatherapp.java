2369454f331e3e5c8ed47de18e71a78836c72946
Sumithra.R <ravikannagi9@gmail.com>
2025-10-13 11:56:10 +0530

    Add files via upload

---
+++ datatypeweatherapp.java
@@ -0,0 +1,11 @@
+public class datatypeweatherapp {
+    public static void main(String[] args){
+        double celsius=36.6;
+        double fahrenheit= (celsius * 1.8) + 32;
+        int DisplayedAs=(int)fahrenheit;
+        System.out.println("===Weather App Temperature===");
+        System.out.println("Celsius:"+celsius);
+        System.out.println("Fahrenheit:"+DisplayedAs);
+
+    }
+}
