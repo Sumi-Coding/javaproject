7f4541787df56b96c945c49f6cda5a020d3cfd57
Sumithra.R <ravikannagi9@gmail.com>
2025-10-14 22:18:00 +0530

    Add files via upload

---
+++ datatypecostingextange.java
@@ -0,0 +1,7 @@
+public class datatypecostingextange {
+    public static void main(String[] args){
+        int num=123;
+        double value=num;
+        System.out.println("this double value"+value);
+    }
+}
