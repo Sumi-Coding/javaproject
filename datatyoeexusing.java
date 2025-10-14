92205528eb85b40d04d2c415954eaed934203ca9
Sumithra.R <ravikannagi9@gmail.com>
2025-10-14 22:17:07 +0530

    Add files via upload

---
+++ datatyoeexusing.java
@@ -0,0 +1,8 @@
+public class datatyoeexusing {
+    public static void main(String[] args){
+        int number=12;
+        int number1=34;
+        int sum=number1+number;
+        System.out.println("enter the int value"+sum);
+    }
+}
