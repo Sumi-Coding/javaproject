2184786ddd180e2d8626ff0957674f20e4cbc7df
Sumithra.R <ravikannagi9@gmail.com>
2025-10-14 22:20:08 +0530

    Add files via upload

---
+++ datatypeintex1.java
@@ -0,0 +1,10 @@
+public class datatypeintex1 {
+    public static void main(String[] args){
+        int value=10;
+        int value1=20;
+        int sum=value+value1;
+        System.out.println("value of a:"+value);
+        System.out.println("value of b:"+value1);
+        System.out.println("sum:"+sum);
+    }
+}
