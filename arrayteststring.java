f8365bc2c731f4890a80521e55338a4fae57e3ed
Sumithra.R <ravikannagi9@gmail.com>
2025-10-15 07:05:46 +0530

    Add files via upload

---
+++ arrayteststring.java
@@ -0,0 +1,9 @@
+public class arrayteststring {
+    public static void main(String[]args){
+         String arr[]={"sumi","sathya","jesi"};
+         for(String i:arr){
+             System.out.println(i);
+
+         }
+    }
+}
