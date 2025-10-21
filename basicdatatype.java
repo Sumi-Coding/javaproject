501b26782c9c1b188ed53e5f60614654513c099b
Sumithra.R <ravikannagi9@gmail.com>
2025-10-21 20:38:04 +0530

    Add files via upload

---
+++ basicdatatype.java
@@ -0,0 +1,22 @@
+public class basicdatatype {
+    public static void main(String[] args){
+        int i=100;
+        short s=30000;
+        double d=457.9;
+        long l=500000000000l;
+        float f=6889.9f;
+        boolean b=true;
+        char c='v';
+        byte value=100;
+        System.out.println("----this is datatype value-----");
+
+        System.out.println("int value:"+i);
+        System.out.println("short value:"+s);
+        System.out.println("double value:"+d);
+        System.out.println("long value:"+l);
+        System.out.println("float value:"+f);
+        System.out.println("boolean value:"+b);
+        System.out.println("char value:"+c);
+        System.out.println("byte value:"+value);
+    }
+}
